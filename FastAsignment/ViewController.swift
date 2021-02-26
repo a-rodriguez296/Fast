196a391bcd04946ca9c5ce3608b1d60d9ac7fa87
Alejandro <alejo80805@hotmail.com>
2021-02-26 17:20:57 -0500

    testing api request. It works!!!!!!!!!

--- FastAsignment/ViewController.swift
+++ FastAsignment/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        ApiClient.shared.execute(request: GetMovies(keyword: "Inception")) { (apiResult: APIResult<Movie>) in
+            switch apiResult {
+            case .success(_, let result):
+                print(result)
+            case .failure(let error):
+                print("\(error.localizedDescription)")
+            case .empty:
+                print("no results")
+            }
+        }
     }
 
 
