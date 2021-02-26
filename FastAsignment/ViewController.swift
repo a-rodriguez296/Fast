d1f9be93718669efa3f2f2ad8c6b0f179c25b952
Alejandro <alejo80805@hotmail.com>
2021-02-26 17:30:49 -0500

    Initial screen

--- FastAsignment/ViewController.swift
+++ FastAsignment/ViewController.swift
@@ -11,20 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        ApiClient.shared.execute(request: GetMovies(keyword: "Inception")) { (apiResult: APIResult<Movie>) in
-            switch apiResult {
-            case .success(_, let result):
-                print(result)
-            case .failure(let error):
-                print("\(error.localizedDescription)")
-            case .empty:
-                print("no results")
-            }
-        }
     }
 
+    @IBAction func didPressContinue(_ sender: Any) {
+
+    }
 
 }
 
