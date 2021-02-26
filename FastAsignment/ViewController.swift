f0eabc3ae3837adaf71a990ce695f36604224f6d
Alejandro <alejo80805@hotmail.com>
2021-02-26 17:03:12 -0500

    Initial Commit

---
+++ FastAsignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastAsignment
+//
+//  Created by Alejandro Rodríguez on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
