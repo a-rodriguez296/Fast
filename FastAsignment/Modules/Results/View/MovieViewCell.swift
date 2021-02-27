d06525eb81ed7b001a2c2d00a35646d95d717a95
Alejandro <alejo80805@hotmail.com>
2021-02-27 13:49:27 -0500

    creating table cell

---
+++ FastAsignment/Modules/Results/View/MovieViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieViewCell.swift
+//  FastAsignment
+//
+//  Created by Alejandro Rodríguez on 27/02/21.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    
+}
